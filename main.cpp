8c4675caea75114b7d9d27bcc8287ea4353b51f3
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-11-24 21:43:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   
+    int n=0, a=1, b=1, c=0;
+    cin >> n;
+
+    cout << a << endl << b << endl;
+
+    for (int i=0; i<=(n/2); i++) {
+
+        c=a+b;
+        a=b;
+        b=c;
+
+        cout << c << endl;
+       
    return 0;
 }
